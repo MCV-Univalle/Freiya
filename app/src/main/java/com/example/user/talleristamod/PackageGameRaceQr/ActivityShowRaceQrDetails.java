870537cd3728466e30b44f855f51696fd2a1ba3e
Zixtor195 <cristian.camilo.jurado@correounivalle.edu.co>
2020-02-26 22:08:06 -0500

    Se realiza Commit para resolver el issue #316

--- app/src/main/java/com/example/user/talleristamod/PackageGameRaceQr/ActivityShowRaceQrDetails.java
+++ app/src/main/java/com/example/user/talleristamod/PackageGameRaceQr/ActivityShowRaceQrDetails.java
@@ -1,15 +1,22 @@
 package com.example.user.talleristamod.PackageGameRaceQr;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.user.talleristamod.PackageGameRaceQr.Questions.ObjectQuestion;
+import com.example.user.talleristamod.PackageProfiles.ProfileEstudiante.ActivityPrincipalProfile;
+import com.example.user.talleristamod.PackageProfiles.ProfileEstudiante.ActivityProfileEstudiante;
+import com.example.user.talleristamod.PackageProfiles.ProfileTallerista.ActivityProfileTallerista;
 import com.example.user.talleristamod.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,7 +26,9 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ActivityShowRaceQrDetails extends AppCompatActivity {
+import static com.example.user.talleristamod.GlobalVariables.GlobalVariables.USER_TYPE;
+
+public class ActivityShowRaceQrDetails extends AppCompatActivity implements View.OnClickListener{
 
     DatabaseRaceQr dataBaseSets;
     ArrayList<ObjectQuestion> questions = new ArrayList<>();
@@ -28,6 +37,7 @@ public class ActivityShowRaceQrDetails extends AppCompatActivity {
     ArrayList<String> questionsQ = new ArrayList<>();
     String nameQrRace;
     TextView textViewNameRace, textViewTitle;
+    ConstraintLayout linearLayoutBack, linearLayoutHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,12 @@ public class ActivityShowRaceQrDetails extends AppCompatActivity {
         textViewTitle = findViewById(R.id.textView15);
         listQuestions = findViewById(R.id.listViewRaceQrDetails);
         listQuestions.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        linearLayoutBack = findViewById(R.id.layoutAtrasT);
+        linearLayoutHome = findViewById(R.id.layoutHomeT);
+
+
+        linearLayoutBack.setOnClickListener(this);
+        linearLayoutHome.setOnClickListener(this);
 
         dataBaseSets = new DatabaseRaceQr(this);
         Bundle parametros = this.getIntent().getExtras();
@@ -77,4 +93,30 @@ public class ActivityShowRaceQrDetails extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layoutAtrasT:
+                finishActivity();
+                break;
+
+            case R.id.layoutHomeT:
+                Intent intent = new Intent(getApplicationContext(), ActivityProfileTallerista.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+
+    private void finishActivity(){
+        Intent intent = new Intent(getApplicationContext(), ActivitySelectRaceQr.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    @Override
+    public void onBackPressed (){
+        finishActivity();
+    }
 }
